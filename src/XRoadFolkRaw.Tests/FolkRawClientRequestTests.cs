ca0640a390b49796017e814b3757668ddc12f4b4
pallrasmussen <41187502+pallrasmussen@users.noreply.github.com>
2025-08-20 09:02:59 +0100

    No unt test

--- src/XRoadFolkRaw.Tests/FolkRawClientRequestTests.cs
+++ src/XRoadFolkRaw.Tests/FolkRawClientRequestTests.cs
@@ -4,60 +4,49 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
-using System.Linq;
 using Xunit;
-using XRoadFolkRaw.Lib;
 
 public class FolkRawClientRequestTests
 {
     private static async Task<(int Port, Task ServerTask, Func<string> GetCapturedRequest)> StartTestServerAsync(string responseBody)
     {
-        TcpListener listener = new(IPAddress.Loopback, 0);
+        var listener = new TcpListener(IPAddress.Loopback, 0);
         listener.Start();
         int port = ((IPEndPoint)listener.LocalEndpoint).Port;
 
         string captured = "";
         async Task Server()
         {
-            using TcpClient client = await listener.AcceptTcpClientAsync();
-            using NetworkStream stream = client.GetStream();
+            using var client = await listener.AcceptTcpClientAsync();
+            using var stream = client.GetStream();
 
-            byte[] buffer = new byte[8192];
+            var buffer = new byte[8192];
             int read;
-            StringBuilder sb = new();
+            var sb = new StringBuilder();
             do
             {
-                read = await stream.ReadAsync(buffer);
-                if (read <= 0)
-                {
-                    break;
-                }
-
+                read = await stream.ReadAsync(buffer, 0, buffer.Length);
+                if (read <= 0) break;
                 sb.Append(Encoding.UTF8.GetString(buffer, 0, read));
-                string current = sb.ToString();
-                int headEnd = current.IndexOf("\r\n\r\n", StringComparison.Ordinal);
+                var current = sb.ToString();
+                var headEnd = current.IndexOf("\r\n\r\n", StringComparison.Ordinal);
                 if (headEnd >= 0)
                 {
-                    string headers = current.Substring(0, headEnd);
-                    int bodyStart = headEnd + 4;
+                    var headers = current.Substring(0, headEnd);
+                    var bodyStart = headEnd + 4;
                     int contentLen = 0;
-                    foreach (string line in headers.Split(["\r\n"], StringSplitOptions.RemoveEmptyEntries))
+                    foreach (var line in headers.Split(new[]{ "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
                     {
                         if (line.StartsWith("Content-Length:", StringComparison.OrdinalIgnoreCase))
                         {
-                            string v = line.Substring("Content-Length:".Length).Trim();
+                            var v = line.Substring("Content-Length:".Length).Trim();
                             int.TryParse(v, out contentLen);
                         }
                     }
                     while (current.Length - bodyStart < contentLen)
                     {
                         read = await stream.ReadAsync(buffer, 0, buffer.Length);
-                        if (read <= 0)
-                        {
-                            break;
-                        }
-
+                        if (read <= 0) break;
                         sb.Append(Encoding.UTF8.GetString(buffer, 0, read));
                         current = sb.ToString();
                     }
@@ -67,8 +56,8 @@ public class FolkRawClientRequestTests
 
             captured = sb.ToString();
 
-            byte[] bodyBytes = Encoding.UTF8.GetBytes(responseBody);
-            string headersOut = "HTTP/1.1 200 OK\r\n" +
+            var bodyBytes = Encoding.UTF8.GetBytes(responseBody);
+            var headersOut = "HTTP/1.1 200 OK\r\n" +
                              "Content-Type: text/xml; charset=utf-8\r\n" +
                              $"Content-Length: {bodyBytes.Length}\r\n" +
                              "Connection: close\r\n\r\n";
@@ -83,7 +72,7 @@ public class FolkRawClientRequestTests
 
     private static string ExtractBody(string rawHttp)
     {
-        int idx = rawHttp.IndexOf("\r\n\r\n", StringComparison.Ordinal);
+        var idx = rawHttp.IndexOf("\r\n\r\n", StringComparison.Ordinal);
         return idx >= 0 ? rawHttp.Substring(idx + 4) : rawHttp;
     }
 
@@ -110,40 +99,40 @@ public class FolkRawClientRequestTests
     }
 
     [Fact]
-    public async Task GetPeoplePublicInfoInsertsTokenAndServiceCode()
+    public async Task GetPeoplePublicInfo_Inserts_Token_And_ServiceCode()
     {
-        (int port, Task serverTask, Func<string> getReq) = await StartTestServerAsync(@"<Envelope><Body><GetPeoplePublicInfoResponse><ok>true</ok></GetPeoplePublicInfoResponse></Body></Envelope>");
-        string url = $"http://127.0.0.1:{port}/";
+        var (port, serverTask, getReq) = await StartTestServerAsync(@"<Envelope><Body><GetPeoplePublicInfoResponse><ok>true</ok></GetPeoplePublicInfoResponse></Body></Envelope>");
+        var url = $"http://127.0.0.1:{port}/";
 
-        string tmp = Path.GetTempFileName();
+        var tmp = Path.GetTempFileName();
         File.WriteAllText(tmp, MinimalGetPeopleTemplate(), Encoding.UTF8);
 
-        FolkRawClient client = new(url, null, TimeSpan.FromSeconds(5), logger: null, verbose: false, maskTokens: false);
+        var client = new FolkRawClient(url, null, TimeSpan.FromSeconds(5), logger:null, verbose:false, maskTokens:false);
 
-        string xml = await client.GetPeoplePublicInfoAsync(
+        var xml = await client.GetPeoplePublicInfoAsync(
             xmlPath: tmp,
             xId: "REQ-9",
             userId: "inspect",
             token: "ABC123TOKEN",
             protocolVersion: "4.0",
             clientXRoadInstance: "FO",
-            clientMemberClass: "GOV",
-            clientMemberCode: "123",
+            clientMemberClass:   "GOV",
+            clientMemberCode:    "123",
             clientSubsystemCode: "CLI",
             serviceXRoadInstance: "FO",
-            serviceMemberClass: "GOV",
-            serviceMemberCode: "321",
+            serviceMemberClass:   "GOV",
+            serviceMemberCode:    "321",
             serviceSubsystemCode: "SRV",
-            serviceCode: "GetPeoplePublicInfo",
-            serviceVersion: "v1",
+            serviceCode:          "GetPeoplePublicInfo",
+            serviceVersion:       "v1",
             ct: default);
 
         await serverTask;
-        string req = getReq();
+        var req = getReq();
 
         Assert.Contains("Content-Type: text/xml", req, StringComparison.OrdinalIgnoreCase);
 
-        string body = ExtractBody(req);
+        var body = ExtractBody(req);
 
         // prefix-agnostic body checks
         Assert.Contains("serviceCode>GetPeoplePublicInfo", body);
@@ -151,188 +140,5 @@ public class FolkRawClientRequestTests
         Assert.Contains("<token>ABC123TOKEN</token>", body);
         Assert.Contains("<ListOfPersonPublicInfoCriteria>", body);
         Assert.Contains("<PersonPublicInfoCriteria", body); // allow self-closing tag e.g., <... />
-    }
-
-    [Fact]
-    public async Task UsesCachedTemplateIfFileDeleted()
-    {
-        string tmp = Path.GetTempFileName();
-        File.WriteAllText(tmp, MinimalGetPeopleTemplate(), Encoding.UTF8);
-
-        (int port, Task serverTask, Func<string> _) = await StartTestServerAsync(@"<Envelope><Body><GetPeoplePublicInfoResponse><ok>true</ok></GetPeoplePublicInfoResponse></Body></Envelope>");
-        string url = $"http://127.0.0.1:{port}/";
-
-        FolkRawClient client = new(url, null, TimeSpan.FromSeconds(5), logger: null, verbose: false, maskTokens: false);
-        client.PreloadTemplates(new[] { tmp });
-        File.Delete(tmp);
-
-        string xml = await client.GetPeoplePublicInfoAsync(
-            xmlPath: tmp,
-            xId: "REQ-99",
-            userId: "inspect",
-            token: "ABC123TOKEN",
-            protocolVersion: "4.0",
-            clientXRoadInstance: "FO",
-            clientMemberClass: "GOV",
-            clientMemberCode: "123",
-            clientSubsystemCode: "CLI",
-            serviceXRoadInstance: "FO",
-            serviceMemberClass: "GOV",
-            serviceMemberCode: "321",
-            serviceSubsystemCode: "SRV",
-            serviceCode: "GetPeoplePublicInfo",
-            serviceVersion: "v1",
-            ct: default);
-
-        await serverTask;
-        Assert.NotNull(xml);
-        Assert.NotEqual(string.Empty, xml);
-    }
-
-    [Fact]
-    public async Task GetPeoplePublicInfoClearsExistingCriteria()
-    {
-        string template = $@"<?xml version=\"1.0\"?>
-<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:prod=\"{ProdNs}\">
-  <soapenv:Header />
-  <soapenv:Body>
-    <prod:GetPeoplePublicInfo>
-      <request>
-        <requestHeader/>
-        <requestBody>
-          <ListOfPersonPublicInfoCriteria>
-            <PersonPublicInfoCriteria><SSN>123</SSN></PersonPublicInfoCriteria>
-            <PersonPublicInfoCriteria><FirstName>Old</FirstName></PersonPublicInfoCriteria>
-          </ListOfPersonPublicInfoCriteria>
-        </requestBody>
-      </request>
-    </prod:GetPeoplePublicInfo>
-  </soapenv:Body>
-</soapenv:Envelope>";
-
-        string tmp = Path.GetTempFileName();
-        File.WriteAllText(tmp, template, Encoding.UTF8);
-
-        (int port, Task serverTask, Func<string> getReq) = await StartTestServerAsync(@"<Envelope><Body><GetPeoplePublicInfoResponse><ok>true</ok></GetPeoplePublicInfoResponse></Body></Envelope>");
-        string url = $"http://127.0.0.1:{port}/";
-
-        FolkRawClient client = new(url, null, TimeSpan.FromSeconds(5), logger: null, verbose: false, maskTokens: false);
-
-        await client.GetPeoplePublicInfoAsync(
-            xmlPath: tmp,
-            xId: "REQ-1",
-            userId: "inspect",
-            token: "ABC123TOKEN",
-            protocolVersion: "4.0",
-            clientXRoadInstance: "FO",
-            clientMemberClass: "GOV",
-            clientMemberCode: "123",
-            clientSubsystemCode: "CLI",
-            serviceXRoadInstance: "FO",
-            serviceMemberClass: "GOV",
-            serviceMemberCode: "321",
-            serviceSubsystemCode: "SRV",
-            serviceCode: "GetPeoplePublicInfo",
-            serviceVersion: "v1",
-            ssn: "999",
-            ct: default);
-
-        await serverTask;
-        string req = getReq();
-        string body = ExtractBody(req);
-
-        XDocument doc = XDocument.Parse(body);
-        XNamespace soapenv = "http://schemas.xmlsoap.org/soap/envelope/";
-        XNamespace prod = ProdNs;
-
-        IEnumerable<XElement>? crits = doc.Root?
-            .Element(soapenv + "Body")?
-            .Element(prod + "GetPeoplePublicInfo")?
-            .Element("request")?
-            .Element("requestBody")?
-            .Element("ListOfPersonPublicInfoCriteria")?
-            .Elements("PersonPublicInfoCriteria");
-
-        Assert.NotNull(crits);
-        List<XElement> list = crits!.ToList();
-        Assert.Single(list);
-        Assert.Equal("999", list[0].Element("SSN")?.Value);
-        Assert.Null(list[0].Element("FirstName"));
-    }
-
-    [Fact]
-    public async Task GetPersonAsyncRemovesLegacyHeaders()
-    {
-        string template = $@"<?xml version=\"1.0\"?>
-<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"
-                  xmlns:prod=\"{ProdNs}\"
-                  xmlns:x=\"http://x-road.eu/xsd/x-road.xsd\"
-                  xmlns:xro=\"http://x-road.eu/xsd/xroad.xsd\"
-                  xmlns:iden=\"http://x-road.eu/xsd/identifiers\">
-  <soapenv:Header>
-    <x:legacy>remove</x:legacy>
-    <xro:service><iden:serviceCode>OLD</iden:serviceCode></xro:service>
-    <xro:client><iden:xRoadInstance>OLD</iden:xRoadInstance></xro:client>
-    <xro:id>OLDID</xro:id>
-    <xro:protocolVersion>OLDPV</xro:protocolVersion>
-  </soapenv:Header>
-  <soapenv:Body>
-    <prod:GetPerson>
-      <request>
-        <requestHeader/>
-        <requestBody/>
-      </request>
-    </prod:GetPerson>
-  </soapenv:Body>
-</soapenv:Envelope>";
-
-        string tmp = Path.GetTempFileName();
-        File.WriteAllText(tmp, template, Encoding.UTF8);
-
-        (int port, Task serverTask, Func<string> getReq) = await StartTestServerAsync(@"<Envelope><Body><GetPersonResponse><ok>true</ok></GetPersonResponse></Body></Envelope>");
-        string url = $"http://127.0.0.1:{port}/";
-
-        FolkRawClient client = new(url, null, TimeSpan.FromSeconds(5), logger: null, verbose: false, maskTokens: false);
-
-        await client.GetPersonAsync(
-            xmlPath: tmp,
-            xId: "REQ-1",
-            userId: "inspect",
-            token: "TOKEN",
-            protocolVersion: "4.0",
-            clientXRoadInstance: "FO",
-            clientMemberClass: "GOV",
-            clientMemberCode: "123",
-            clientSubsystemCode: "CLI",
-            serviceXRoadInstance: "FO",
-            serviceMemberClass: "GOV",
-            serviceMemberCode: "321",
-            serviceSubsystemCode: "SRV",
-            serviceCode: "GetPerson",
-            serviceVersion: "v1",
-            ct: default);
-
-        await serverTask;
-        string req = getReq();
-        string body = ExtractBody(req);
-
-        XDocument doc = XDocument.Parse(body);
-        XNamespace soapenv = "http://schemas.xmlsoap.org/soap/envelope/";
-        XNamespace x = "http://x-road.eu/xsd/x-road.xsd";
-        XNamespace xro = "http://x-road.eu/xsd/xroad.xsd";
-        XNamespace iden = "http://x-road.eu/xsd/identifiers";
-
-        XElement header = doc.Root!.Element(soapenv + "Header")!;
-        Assert.Null(header.Element(x + "legacy"));
-        Assert.Equal("REQ-1", header.Element(xro + "id")?.Value);
-        Assert.Equal("4.0", header.Element(xro + "protocolVersion")?.Value);
-        Assert.Equal("inspect", header.Element(x + "userId")?.Value);
-        XElement serviceEl = header.Element(xro + "service")!;
-        Assert.Equal("GetPerson", serviceEl.Element(iden + "serviceCode")?.Value);
-        XElement clientEl = header.Element(xro + "client")!;
-        Assert.Equal("FO", clientEl.Element(iden + "xRoadInstance")?.Value);
-        string headerString = header.ToString();
-        Assert.DoesNotContain("OLD", headerString);
-        Assert.DoesNotContain("OLDID", headerString);
     }
 }
