5bd3f28fbdf799dbfa2abab3af2e12924897768b
pallrasmussen <41187502+pallrasmussen@users.noreply.github.com>
2025-08-21 00:04:34 +0100

    chore: remove redundant system usings

--- src/XRoadFolkRaw.Tests/FolkRawClientRequestTests.cs
+++ src/XRoadFolkRaw.Tests/FolkRawClientRequestTests.cs
@@ -1,9 +1,6 @@
-using System;
-using System.IO;
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 public class FolkRawClientRequestTests
