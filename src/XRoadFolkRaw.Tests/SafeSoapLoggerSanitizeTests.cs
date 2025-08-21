0c7a8a1b50878ab88c38e3dea82865d2a0da96c8
pallrasmussen <41187502+pallrasmussen@users.noreply.github.com>
2025-08-21 01:05:54 +0100

    Use GeneratedRegex for sanitize test patterns

--- src/XRoadFolkRaw.Tests/SafeSoapLoggerSanitizeTests.cs
+++ src/XRoadFolkRaw.Tests/SafeSoapLoggerSanitizeTests.cs
@@ -2,7 +2,7 @@ using System.Text.RegularExpressions;
 using XRoadFolkRaw.Lib.Logging;
 using Xunit;
 
-public class SafeSoapLoggerSanitizeTests
+public partial class SafeSoapLoggerSanitizeTests
 {
     [Fact]
     public void SanitizesSensitiveElementsAndAttributes()
@@ -17,10 +17,25 @@ public class SafeSoapLoggerSanitizeTests
         Assert.DoesNotContain("password=\"secret123\"", sanitized, StringComparison.OrdinalIgnoreCase);
         Assert.DoesNotContain("apikey=\"apikey123\"", sanitized, StringComparison.OrdinalIgnoreCase);
 
-        Assert.Matches(new Regex("username>\\*+ce<", RegexOptions.IgnoreCase), sanitized);
-        Assert.Matches(new Regex("password>\\*+rd<", RegexOptions.IgnoreCase), sanitized);
-        Assert.Matches(new Regex("token>\\*+EN<", RegexOptions.IgnoreCase), sanitized);
-        Assert.Matches(new Regex("password=\\"\\*+23\\"", RegexOptions.IgnoreCase), sanitized);
-        Assert.Matches(new Regex("apikey=\\"\\*+23\\"", RegexOptions.IgnoreCase), sanitized);
+        Assert.Matches(UsernameRegex(), sanitized);
+        Assert.Matches(PasswordRegex(), sanitized);
+        Assert.Matches(TokenRegex(), sanitized);
+        Assert.Matches(PasswordAttributeRegex(), sanitized);
+        Assert.Matches(ApiKeyAttributeRegex(), sanitized);
     }
+
+    [GeneratedRegex("username>\\*+ce<", RegexOptions.IgnoreCase)]
+    private static partial Regex UsernameRegex();
+
+    [GeneratedRegex("password>\\*+rd<", RegexOptions.IgnoreCase)]
+    private static partial Regex PasswordRegex();
+
+    [GeneratedRegex("token>\\*+EN<", RegexOptions.IgnoreCase)]
+    private static partial Regex TokenRegex();
+
+    [GeneratedRegex("password=\\\"\\*+23\\\"", RegexOptions.IgnoreCase)]
+    private static partial Regex PasswordAttributeRegex();
+
+    [GeneratedRegex("apikey=\\\"\\*+23\\\"", RegexOptions.IgnoreCase)]
+    private static partial Regex ApiKeyAttributeRegex();
 }
