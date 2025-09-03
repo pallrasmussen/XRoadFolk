f533d66d2e8998a96137c056919e3567f2b88f53
Pall Rasmussen <41187502+pallrasmussen@users.noreply.github.com>
2025-09-03 23:21:50 +0100

    Tests project

---
+++ tests/XRoadFolkWeb.Tests/PeopleResponseParserTests.cs
@@ -0,0 +1,66 @@
+using Microsoft.Extensions.Logging.Abstractions;
+using XRoadFolkWeb.Features.People;
+using Xunit;
+
+namespace XRoadFolkWeb.Tests;
+
+public class PeopleResponseParserTests
+{
+    [Fact]
+    public void ParsePeopleList_Finds_Persons_Ignoring_Namespaces()
+    {
+        string xml = """
+            <s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'>
+              <s:Body>
+                <x:GetPeoplePublicInfoResponse xmlns:x='urn:test'>
+                  <x:ListOfPersonPublicInfo>
+                    <x:PersonPublicInfo>
+                      <x:PublicId>123</x:PublicId>
+                      <x:Names>
+                        <x:Name>
+                          <x:Type>FirstName</x:Type>
+                          <x:Order>1</x:Order>
+                          <x:Value>Jane</x:Value>
+                        </x:Name>
+                        <x:Name>
+                          <x:Type>LastName</x:Type>
+                          <x:Value>Doe</x:Value>
+                        </x:Name>
+                      </x:Names>
+                      <x:SSN>010203123</x:SSN>
+                    </x:PersonPublicInfo>
+                  </x:ListOfPersonPublicInfo>
+                </x:GetPeoplePublicInfoResponse>
+              </s:Body>
+            </s:Envelope>
+        """;
+        var parser = new PeopleResponseParser(NullLogger<PeopleResponseParser>.Instance);
+        var rows = parser.ParsePeopleList(xml);
+        Assert.Single(rows);
+        Assert.Equal("123", rows[0].PublicId);
+        Assert.Equal("Jane", rows[0].FirstName);
+        Assert.Equal("Doe", rows[0].LastName);
+        Assert.Equal("010203123", rows[0].SSN);
+    }
+
+    [Fact]
+    public void FlattenResponse_Produces_KeyValue_Pairs()
+    {
+        string xml = """
+            <s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'>
+              <s:Body>
+                <x:GetPeoplePublicInfoResponse xmlns:x='urn:test'>
+                  <X>
+                    <A>1</A>
+                    <B>2</B>
+                  </X>
+                </x:GetPeoplePublicInfoResponse>
+              </s:Body>
+            </s:Envelope>
+        """;
+        var parser = new PeopleResponseParser(NullLogger<PeopleResponseParser>.Instance);
+        var pairs = parser.FlattenResponse(xml);
+        Assert.Contains(pairs, p => p.Key.EndsWith("X.A") && p.Value == "1");
+        Assert.Contains(pairs, p => p.Key.EndsWith("X.B") && p.Value == "2");
+    }
+}
